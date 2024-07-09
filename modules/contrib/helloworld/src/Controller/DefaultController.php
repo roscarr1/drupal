761d837c6cfb5eadd0c7be96264f1828b4df6128
roscarr1 <48859886+roscarr1@users.noreply.github.com>
2024-07-09 19:32:22 +0200

    go 1
    go 1

--- modules/contrib/helloworld/src/Controller/DefaultController.php
+++ modules/contrib/helloworld/src/Controller/DefaultController.php
@@ -16,9 +16,10 @@ class DefaultController extends ControllerBase {
    *   Return Hello string.
    */
   public function hello() {
+    $ola='perro';
     return [
       '#type' => 'markup',
-      '#markup' => $this->t('<h2>Hello World (Example Drupal 8 Controller) code:</h2>')
+      '#markup' => $this->t(<?php echo 'perro'; ?>'<h2>Hello World (Example Drupal 8 Controller) code:</h2>')
       . '<code><?php<br>namespace Drupal\helloworld\Controller;<br>'
       . 'use Drupal\Core\Controller\ControllerBase;<br><br>'
       . 'class DefaultController extends ControllerBase {<br>'
@@ -26,7 +27,44 @@ class DefaultController extends ControllerBase {
       . '&nbsp;&nbsp;&nbsp;&nbsp;return [<br>'
       . '&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;\'#type\' => \'markup\',<br>'
       . '&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;\'#markup\' => $this->t(\'Hello World (Example Drupal 10 Controller) code!\')<br>'
-      . '&nbsp;&nbsp;&nbsp;&nbsp;];<br>&nbsp;&nbsp;}<br>}</code>',
+      . '&nbsp;&nbsp;&nbsp;&nbsp;];<br>&nbsp;&nbsp;}<br>}</code><br>'
+      . '<!DOCTYPE html>
+          <html lang="es">
+          <head>
+              <meta charset="UTF-8">
+              <meta name="viewport" content="width=device-width, initial-scale=1.0">
+              <title>Tabla de demostración</title>
+          </head>
+          <body>
+              <table>
+                  <thead>
+                      <tr>
+                          <th>Nombre</th>
+                          <th>Apellido</th>
+                          <th>Edad</th>
+                      </tr>
+                  </thead>
+                  <tbody>
+                      <tr>
+                          <td>Juan</td>
+                          <td>Pérez</td>
+                          <td>30</td>
+                      </tr>
+                      <tr>
+                          <td>María</td>
+                          <td>Gómez</td>
+                          <td>25</td>
+                      </tr>
+                      <tr>
+                          <td>Pedro</td>
+                          <td>López</td>
+                          <td>40</td>
+                      </tr>
+                  </tbody>
+              </table>
+          </body>
+          </html>
+        ',
     ];
   }
 
